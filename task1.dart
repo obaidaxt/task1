de74394ea9cc7daf4fafcbc140425ebbf882fa5e
obaidaxt <obaidaxt@gmail.com>
2023-03-21 20:55:19 +0100

    first commit

---
+++ task1.dart
@@ -0,0 +1,54 @@
+// 1- What is programming?
+// programming is the pprocess of comminicate with computer to perform any task in form of application help us in our daily life,it starts with definig the problem and then design the solution lastly implement the solution.
+// 2- What is programming Applications?
+// a software application to perform any task we ask to do from very basic application like creating application to add the sum of two numbers to a very complex application like playing with computer in virtual world.
+// 3- With example, give a def. about programming languages?
+// it is a ser of instructions can be understood by computer every programming language has is designed for specefic reason like dart for web and app mobile development
+// 4- Compare between low level languages and high-level languages?
+//  low level languages are more difficult to write and debug, but provide direct access to the hardware and are very efficient. High level languages are easier to write and debug but they can be used for different purpose like mobile web applications
+// 5- What is meant by Pseudocode?
+// Pseudocode is algorithmus to write the code in more readable way for human and it can be used for any programming language.
+// 6- Write a short brief about Mobile application development.
+//  Mobile application is a software applications that run on mobile devices such as smartphones, tablets.Mobile apps can be developed for different operating systems such as iOS and Android, and there are two approaches : native app development and hybrid or cross app development. Native app development involves building an app specifically for a single operating system using the platform's native tools and language, while hybrid and cross app development involves building a single app that can run on multiple platforms using web technologies like HTML, CSS, and JavaScript or using flutter or react native ...
+// 7- What is meant by framework? And what is the difference between it and programming languages?
+// framework is the enironment wehre the Programming language can run and performer any task
+// 1- Every Dart statement ends with…semicolumn;
+// 2- is a set of rules to order the methematicals operations the order as follows from start
+// Parentheses ( )
+// Exponentiation (power) ( ** )
+// Multiplication ( * ) and division ( / , // , % )
+// Addition ( + ) and subtraction ( - )   
+// 3- main() fundtion
+// 4-for whole number we use integer int
+// 5-String ="employee_name"
+// 6- int shirt-size=7
+// String shirt-size="list of number"
+// 7- int age=33
+// 8-double pi=3.14
+// 9- both after used cannot change the value the main difference between 'const' and 'final' .. when you want to assign constant value  to variable at compile time , you should go to use 'const' keyword , otherwise if you would assign a constant value later on at runtime you should go to use 'final' keyword
+// 10-the key difference between var and dynamic is that var is used to declare a variable with an inferred type so it will determined which type of value depend on the sort , while dynamic is used to declare a variable with a type that can change at runtime
+// 11-    3*5*8+4*(2+5)/5=124.4
+// (1+1)*5+5=15
+//  6/2 + 6*6=39
+//  7+2 +9^2=90
+// 12-
+//  what cause error in this code?
+// only semicolumn musst add ;
+// 13- what is the output of this code?
+// a. 
+// hello
+// world
+// b
+// *****      *****
+// c
+// *****
+// ****
+// ***
+// **
+// *
+// d.
+// 5
+// 6
+// 7
+// 12
+// 14 
